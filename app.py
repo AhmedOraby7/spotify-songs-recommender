0100bbec750c0718ff1f9c03d4eb397dc7f69dee
Ahmed Ali <oraby.ali@rwth-aachen.de>
2022-12-23 17:04:10 +0100

    adding background image

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import streamlit.components.v1 as components
 
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+      background-image: url("data:image/png;base64,%s");
+      background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
+
 genre_names = ['Dance Pop', 'Electronic', 'Electropop', 'Hip Hop', 'Jazz', 'Latin', 'Pop', 'Pop Rap', 'R&B', 'Rock']
 audio_feats = ["acousticness", "danceability", "energy", "instrumentalness", "valence", "tempo"]
 
